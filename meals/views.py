46abce0bb6ec74682a5abbfc6d22de9d7ac67931
smailAA <Zyuxin719@sjtu.edu.cn>
2023-06-03 19:49:47 +0800

    修改

--- meals/views.py
+++ meals/views.py
@@ -12,16 +12,9 @@ def index(request):
         return redirect('/meals/login/')
     else:
         #修改索引超出问题
-        most_likes_meal = Meal.objects.order_by("-likes")[:3]  # 获取点赞数最多的前三个菜品
-        content = {'meal1': None, 'meal2': None, 'meal3': None}
-        if most_likes_meal:
-            content['meal1'] = most_likes_meal[0]
-        if len(most_likes_meal) > 1:
-            content['meal2'] = most_likes_meal[1]
-        if len(most_likes_meal) > 2:
-            content['meal3'] = most_likes_meal[2]
+        most_likes_meal = Meal.objects.filter().order_by("-likes")  # 按点赞数降序排列菜品
+        content = {'meal1': most_likes_meal[0], 'meal2': most_likes_meal[1], 'meal3': most_likes_meal[2]}
         return render(request, 'meals/index.html', content)
-
 
 def search_result(request):
     if not request.session.get('is_login', None):  # 如果未登录，跳转到登录页面
